280d9af716fc039a0ea93b6f8bcfd579c30d3bd4
Olivier Perriot <olivier.perriot@free.fr>
2018-12-22 22:46:20 +0000

    Move specific work about ansible in wapi

--- bin/filters/liftree_filters.py
+++ bin/filters/liftree_filters.py
@@ -1,8 +1,18 @@
 import yaml
 import json
+import os
+import markdown
 
 def filter_to_yaml(input):
     return yaml.dump(input, default_flow_style=False)
+
+def filter_to_markdown(value):
+    extensions = ['extra', 'smarty']
+    return markdown.markdown(value, extensions=extensions, output_format='html5')
+
+def filter_basename(path):
+     return os.path.basename(path)
+
 
 def filter_flat(default, sep=','):
     if default is None:
@@ -12,20 +22,14 @@ def filter_flat(default, sep=','):
     else:
         return default
 
-def filter_selected_options(value):
+def filter_to_sui_options(value, selected=False):
     if value is None:
         selected_options = []
     elif isinstance(value, list):
         selected_options = [
-            dict(name=option, value=option, selected=True)
+            dict(name=option, value=option, selected=selected)
             for option in value
         ]
     else:
-        selected_options = [dict(name=value, value=value, selected=True)]
+        selected_options = [dict(name=value, value=value, selected=selected)]
     return json.dumps(selected_options)
-
-def filter_get(my_list, value):
-    if my_list is None:
-        return ''
-    else:
-        return value if value in my_list else ''
