43d3602a8f126e2dc1d6ea80476986e884917222
Olivier Perriot <olivier.perriot@free.fr>
2019-01-05 23:12:54 +0000

    Comment trace in unittest

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -12,19 +12,19 @@ class TestLiftreeLoaders(unittest.TestCase):
 
     def test_git(self):
         path =f'{liftree_tests.LIFTREE_PATH_TEST}/data/test.secret'
-        print(path)
+#         print(path)
         os.system(f'git checkout {path}')
         params = dict(
             repo = liftree_tests.LIFTREE_PATH_ROOT
         )
         data = liftree.loaders.get_git_info.get_data(path, params)
-        pprint.pprint(data)
+#         pprint.pprint(data)
         self.assertEqual(data['status'], 'Unknown')
         self.assertFalse(data['untracked'])
         with open(path, 'a') as fst:
             fst.write("new text fot unittest")
         data = liftree.loaders.get_git_info.get_data(path, params)
-        pprint.pprint(data)
+#         pprint.pprint(data)
         self.assertEqual(data['status'], 'M')
         self.assertFalse(data['untracked'])
         os.system(f'git checkout {path}')
@@ -34,7 +34,7 @@ class TestLiftreeLoaders(unittest.TestCase):
         with open(path, "w") as fst:
             fst.write("new text fot unittest")
         data = liftree.loaders.get_git_info.get_data(path, params)
-        pprint.pprint(data)
+#         pprint.pprint(data)
         self.assertEqual(data['status'], 'Unknown')
         self.assertTrue(data['untracked'])
         os.remove(path)
