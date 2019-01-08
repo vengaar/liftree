be5fdf5333bbda7cf7d2ef94ef9b358f32b13d2f
Olivier Perriot <olivier.perriot@free.fr>
2019-01-08 21:53:57 +0000

    Add .mypy_cache/ in .gitignore and complete loader tests

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,14 +1,43 @@
 import unittest, pprint
 import os
+from bs4 import BeautifulSoup
 
 # liftree import
 import sys
 sys.path.append('.')
 import liftree
 import liftree.loaders.get_git_info
+import liftree.loaders.file_json_loader
+import liftree.loaders.file_yaml_loader
+import liftree.loaders.file_markdown_loader
 import tests as liftree_tests
 
 class TestLiftreeLoaders(unittest.TestCase):
+
+    def test_json(self):
+        path =f'{liftree_tests.LIFTREE_PATH_TEST}/data/test.json'
+        params = dict()
+        data = liftree.loaders.file_json_loader.get_data(path, params)
+#         pprint.pprint(data)
+        self.assertEqual(data['markers'][1]['name'], 'Shangri-La Hotel')
+
+    def test_yaml(self):
+        path =f'{liftree_tests.LIFTREE_PATH_TEST}/data/test.yaml'
+        params = dict()
+        data = liftree.loaders.file_yaml_loader.get_data(path, params)
+#         pprint.pprint(data)
+        self.assertEqual(data['items'][1]['part_no'], 'E1628')
+
+    def test_markdown(self):
+        path =f'{liftree_tests.LIFTREE_PATH_TEST}/data/test.md'
+        params = dict()
+        data = liftree.loaders.file_markdown_loader.get_data(path, params)
+#         pprint.pprint(data)
+        soup = BeautifulSoup(data, 'html.parser')
+#         print(soup.prettify())
+        h1 = soup.h1.string
+#         print(h1)
+        self.assertEqual(h1, 'Markdown: Syntax')
 
     def test_git(self):
         path =f'{liftree_tests.LIFTREE_PATH_TEST}/data/test.secret'
