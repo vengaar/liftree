0405b9dd2914fb98986f4b1e26f3150e0d2db25a
Olivier Perriot <olivier.perriot@free.fr>
2019-02-12 22:08:36 +0000

    Update enconding for markdown

--- liftree/loaders/file_markdown_loader.py
+++ liftree/loaders/file_markdown_loader.py
@@ -1,8 +1,10 @@
 import markdown
+import codecs
 
 def get_data(path, params):
     extensions = ['extra', 'smarty']
-    with open(path, 'r') as stream:
+    with codecs.open(path, mode="r", encoding="utf-8") as stream:
+#     with open(path, 'r') as stream:
         md = stream.read()
     data = markdown.markdown(md, extensions=extensions, output_format='html5')
     return data
